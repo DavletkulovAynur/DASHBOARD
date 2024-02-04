0ee14a7790dbcf9040e5283cc01d8dcddc11b080
afdavletkulov <afdavletkulov@rttv.ru>
2024-02-04 14:36:05 -0300

    Dashboard, init

---
+++ src/containers/Dashboard.tsx
@@ -0,0 +1,16 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <div className="dashboard">
+        <div className="balance">
+            Your Balance: $1,000.00
+        </div>
+        <div className="currency-rate">
+            Current Currency Rate: USD to EUR - 1.18
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "../containers/Dashboard";
+
+const Main = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Main;
