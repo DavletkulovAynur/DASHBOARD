2ca6fd44fcd023b58ea2e7f857dbe827c963b1ac
afdavletkulov <afdavletkulov@rttv.ru>
2024-02-07 18:02:26 -0300

    DOA2, cdd, variables, module, import

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,16 +1,16 @@
+import css from "./dashboard.module.css";
+
 const Dashboard = () => {
   return (
     <div>
-      <div className="dashboard">
-        <div className="balance">
-            Your Balance: $1,000.00
-        </div>
+      <div className={css.dashboard}>
+        <div className="balance">Your Balance: $1,000.00</div>
         <div className="currency-rate">
-            Current Currency Rate: USD to EUR - 1.18
+          Current Currency Rate: USD to EUR - 1.18
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
